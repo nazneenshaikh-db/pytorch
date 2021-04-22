299b87042e394a52fd4640428ce22ab4b2618625
Tugsbayasgalan Manlaibaatar <tmanlaibaatar@fb.com>
2021-04-22 14:29:45 -0700

    torch.jit.ignore as a context manager
    ghstack-source-id: 3cb2c1d9d059b67468bd19484c9c40e71484ab1e
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/55172

--- torch/jit/frontend.py
+++ torch/jit/frontend.py
@@ -1,10 +1,11 @@
+import os
 import torch
 import sys
 import ast
 import inspect
 import string
 from textwrap import dedent
-from typing import List
+from typing import List, Tuple  # noqa: F401
 from torch._C._jit_tree_views import (
     ClassDef, Ident, Stmt, Decl, Def, Var,
     EmptyTypeAnnotation, Param, ExprStmt, Assign,
@@ -18,7 +19,7 @@ from torch._C._jit_tree_views import (
 )
 from torch._utils_internal import get_source_lines_and_file
 
-from torch._jit_internal import SourceContext, should_drop, is_static_fn
+from torch._jit_internal import SourceContext, should_drop, is_static_fn, FunctionModifiers  # noqa: F401
 import torch.jit.annotations
 
 # Borrowed from cPython implementation
@@ -291,6 +292,20 @@ def get_jit_def(fn, def_name, self_name=None, is_classmethod=False):
 
     return build_def(ctx, fn_def, type_line, def_name, self_name=self_name)
 
+def is_torch_jit_ignore_context_manager(stmt):
+    # checks if the statement is torch.jit.ignore context manager
+    if isinstance(stmt.items[0].context_expr, ast.Call):
+        # extract torch part
+        function = stmt.items[0].context_expr.func
+        if isinstance(function, ast.Attribute):
+            attr_name = function.attr
+            attr_value = function.value
+            if attr_name == "ignore" and isinstance(attr_value, ast.Attribute):
+                # there should be at most two nested attributes (e.g torch.jit.ignore)
+                if attr_value.attr == "jit" and isinstance(attr_value.value, ast.Name):
+                    if attr_value.value.id == "torch":
+                        return True
+    return False
 
 class Builder(object):
     def __call__(self, ctx, node):
@@ -363,6 +378,94 @@ def build_param(ctx, py_arg, self_name, kwarg_only):
         annotation_expr = EmptyTypeAnnotation(r)
     return Param(annotation_expr, Ident(r, name), kwarg_only)
 
+def build_ignore_context_manager(ctx, stmt):
+    def process_ins_outs(args):
+        # parse the context manager to figure out inputs and outputs
+        # with their annotated types
+        inputs = []
+        outputs = []
+        for arg in args:
+            var_name = arg.arg
+            var_ann = arg.value.value
+            var_decl_type, var_ann = var_ann.split(":")
+            if var_decl_type == "inp":
+                inputs.append((var_name, var_ann))
+            if var_decl_type == "out":
+                outputs.append((var_name, var_ann))
+        return inputs, outputs
+
+    def create_unique_name_ext(ctx, stmt):
+        # extension will be based on the filename plus the line number
+        # of original context manager
+        return os.path.basename(ctx.filename).replace(".", "_") + "_" + str(stmt.lineno)
+
+    def build_return_ann_stmt(outputs):
+        return_type_ann = ""
+        return_statement_str = "return "
+        if len(outputs) == 0:
+            return_type_ann += " -> None: pass"
+        if len(outputs) == 1:
+            return_type_ann = " -> " + outputs[0][1] + ": pass"
+            return_statement_str += outputs[0][0]
+        if len(outputs) > 1:
+            return_type_ann = "Tuple["
+            for var in outputs:
+                var_name, var_ann = var
+                return_type_ann += var_ann + ", "
+                return_statement_str += var_name + ", "
+            return_type_ann = " -> " + return_type_ann[:-2] + "]" + ": pass"
+            return_statement_str = return_statement_str[:-2]
+        return return_type_ann, return_statement_str
+
+    def build_args(args):
+        result = ""
+        for arg in args:
+            var_name, _ = arg
+            result += var_name + ", "
+        return result[:-2]
+
+    inputs, outputs = process_ins_outs(stmt.items[0].context_expr.keywords)
+
+    # build the replacement function str with given inputs and outputs
+    ignore_function_name = "func_ignore_" + create_unique_name_ext(ctx, stmt)
+    ignore_function_str = "\ndef " + ignore_function_name + "("
+    for var in inputs:
+        var_name, var_ann = var
+        ignore_function_str += var_name + " :" + var_ann + ", "
+    ignore_function_str = ignore_function_str[:-2] + ")"
+
+    return_ann, return_stmt = build_return_ann_stmt(outputs)
+    ignore_function_str += return_ann
+
+    # first create the functionDef object from just declaration
+    ignore_function = ast.parse(ignore_function_str).body[0]
+
+    # dump the body of context manager to dummy function
+    ignore_function.body = stmt.body
+
+    # insert return statement to the function
+    return_stmt = ast.parse(return_stmt).body[0]
+    ignore_function.body.append(return_stmt)
+
+    # registers the custom function in the global context
+    ignore_func_str = ast.unparse(ignore_function)
+    ignore_func_str += "\nglobals()[\"{}\"] = {}".format(ignore_function_name, ignore_function_name)
+    ignore_func_str += "\nglobals()[\"{}\"]._torchscript_modifier = FunctionModifiers.IGNORE".format(ignore_function_name)
+    exec(ignore_func_str)  # noqa: P204
+
+    # build the statements as:
+    # <out_1>, <out_2>, ... = torch.jit.frontend.<func>(<in_1>, <in_2>)
+    assign_str_lhs = build_args(outputs)
+    # this function will be registered in torch.jit.frontend module by default
+    assign_str_rhs = "torch.jit.frontend.{}(".format(ignore_function_name) + build_args(inputs) + ")"
+
+    if len(outputs) > 0:
+        assign_str = assign_str_lhs + " = " + assign_str_rhs
+    else:
+        assign_str = assign_str_rhs
+
+    assign_ast = ast.parse(assign_str).body[0]
+    return assign_ast
 
 def get_default_args(fn):
     if fn is None:
@@ -544,6 +647,12 @@ class StmtBuilder(Builder):
     @staticmethod
     def build_With(ctx, stmt):
         r = ctx.make_range(stmt.lineno, stmt.col_offset, stmt.col_offset + len("with"))
+        # Handle ignore context manager
+        if is_torch_jit_ignore_context_manager(stmt):
+            if sys.version_info < (3, 9):
+                raise NotSupportedError(r, "torch.jit.ignore context manager is only supported in 3.9")
+            assign_ast = build_ignore_context_manager(ctx, stmt)
+            return build_stmt(ctx, assign_ast)
         return With(r, build_withitems(ctx, stmt.items), build_stmts(ctx, stmt.body))
 
 class ExprBuilder(Builder):
